ce972bf1279b89b60218e21c9b98eb7535fe2d85
Mickey Phillips <phillipsmickey41@gmail.com>
2021-08-28 12:09:30 -0500

    decision prompts are working

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,6 +1,7 @@
 // Assignment code here
-
-
+var letters = [];
+var numbers = [];
+var special = [];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -12,6 +13,52 @@ function writePassword() {
   passwordText.value = password;
 
 }
- 
+function generatePassword() {
+  var lengthPrompt = window.prompt("Password Length?(8-128)");
+  var length = parseInt(lengthPrompt);
+  if (length >= 8 && length <= 128) {
+    console.log("alien-spaceship");
+    lowCase();
+  } 
+  else {
+    console.log("garlic");
+    return generatePassword();
+
+  }
+}
+function lowCase() {
+  var lowC = window.prompt(("Lower Case?(Y/N)").toLowerCase());
+  if (lowC === "y") {
+    console.log("bet");
+  }
+  else if (lowC === "n") {
+    console.log("cheese");
+  }
+  else {
+    console.log("Congress");
+    return lowCase();
+  }
+
+}
+function upperCase() {
+  var upperC = window.prompt(("Upper Case?(Y/N)").toLowerCase());
+  if (upperC === "y") {
+    console.log("epic");
+  }
+  else if (upperC === "n") {
+    console.log("lazers");
+  }
+  else {
+    console.log("California");
+    return upperC;
+  }
+
+}
+function numbers() {
+
+}
+function symbols() {
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
